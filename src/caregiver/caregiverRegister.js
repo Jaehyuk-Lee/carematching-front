7e1d42b09c1fea973144cd8ca4e4b7cd4917657a
jeeyun2 <irisjeeyun@naver.com>
2025-02-27 16:06:44 +0900

    use client 제거

--- src/caregiver/caregiverRegister.js
+++ src/caregiver/caregiverRegister.js
@@ -1,5 +1,3 @@
-"use client"
-
 import styles from './caregiverEdit.module.css';
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
--- src/caregiver/caregiverUpdate.js
+++ src/caregiver/caregiverUpdate.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import styles from './caregiverEdit.module.css';
