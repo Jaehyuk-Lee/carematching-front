df840ca4e8b05af7252f17accfd751d475a96020
Lee Jaehyuk <alex00728@outlook.com>
2025-03-05 15:48:03 +0900

    Router 모듈화 (#21)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './user/Login';
-import Signup from './user/Signup';
-import MyPage from './user/MyPage';
-import CaregiverListPage from './caregiver/caregiverList';
-import CaregiverDetailPage from './caregiver/caregiverDetail';
-import Cert from './admin/Cert';
 import AppProvider from './context/AppProvider';
 import LoadingSpinner from './components/LoadingSpinner';
 import styles from './App.module.css';
-import Community from './community/Community';
+import AppRouter from './router';
 
 function App() {
   return (
@@ -22,16 +15,7 @@ function App() {
           <Header />
           <LoadingSpinner />
           <main className={styles.mainContent}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/mypage/*" element={<MyPage />} />
-              <Route path="/caregiver" element={<CaregiverListPage />} />
-              <Route path="/caregiver/:id" element={<CaregiverDetailPage />} />
-              <Route path="/admin/cert" element={<Cert />} />
-              <Route path="/community/*" element={<Community />} />
-            </Routes>
+            <AppRouter />
           </main>
           <Footer />
         </div>
