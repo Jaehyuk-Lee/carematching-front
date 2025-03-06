384d4a78f484ea6554a3d6bf821bae33499fcdc0
alljju <cyjj625@gmail.com>
2025-03-06 21:45:44 +0900

    feat:메인이랑 merge하기전 warning해결

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -141,11 +141,6 @@ function Home() {
     }
   ];
 
-  const handleLogout = () => {
-    // 로그아웃 로직을 구현 후 로그인 페이지로 이동합니다.
-    logout();
-    navigate('/login');
-  };
 
   const handleAdminFeature = () => {
     navigate('/admin/cert');
