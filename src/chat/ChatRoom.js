1c2fdcb5c8582211354efd5bd000feb7026c8877
Jaehyuk-Lee <alex00728@outlook.com>
2025-03-08 21:19:27 +0900

    fix ws error

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -41,7 +41,7 @@ const ChatRoom = ({ roomId, onBack, onClose, chatRooms }) => {
   }, []);
 
   const connectWebSocket = useCallback(() => {
-    const socket = new SockJS("http://localhost:8080/wss");
+    const socket = new SockJS(config.apiUrl + "/wss");
     stompClient = Stomp.over(socket);
     stompClient.connect({}, onConnected, onError);
   }, [onConnected, onError]);
